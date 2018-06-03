3944e28c15f511d2558c90623572e723f759fa47
adapass182 <adampassingham90@gmail.com>
2018-06-03 17:02:03 +0200

    toppings now update price correctly

--- src/reducers/toppingState_r.js
+++ src/reducers/toppingState_r.js
@@ -1,11 +1,11 @@
-export default (state = [0], { type, payload } = {}) => {
+export default (state = [], { type, payload } = {}) => {
     
     switch (type) {
         case 'ADD_TOPPING':
             state = state.concat(parseFloat(payload))
             return state
         case 'REMOVE_TOPPING':
-            state = state.splice(-1, 1)
+            state = state.slice(0, (state.length - 1))
             return state
     default:
         return state
